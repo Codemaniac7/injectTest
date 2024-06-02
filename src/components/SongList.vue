552427c4d94de861f614fd09f3eff3f743655c68
Jung Seung Min <jsm1592@gmail.com>
2024-06-02 16:07:04 +0900

    injectTest

---
+++ src/components/SongList.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <!-- <li v-for="s in songs"></li> -->
+        
+        <SongListItem v-for="s in songs" :key="s.id" :song="s"/>
+        <div>체크된 곡 수 : {{ doneCount }}</div>
+    </ul>
+
+</template>
+
+<script>
+import SongListItem from './SongListItem.vue';
+export default {
+    name:'SongList',
+    components: {SongListItem},
+    props: ['songs'],
+    inject: ['doneCount']
+}
+</script>
